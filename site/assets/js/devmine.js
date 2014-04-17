e579b0fa6284e00bb051ec23b6d1f12b3c40cde5
Laurent Weingart <laurent.weingart@epfl.ch>
2014-04-17 10:48:52 +0200

    modifies bootstrap touchspin to use string lists

---
+++ site/assets/js/devmine.js
@@ -0,0 +1,7 @@
+var languages = new Array();
+languages[0] = "Java";
+languages[1] = "Python";
+languages[2] = "C++";
+languages[3] = "Haskell";
+
+var paradigms = new Array("OOP", "Functional", "Procedural", "Logic", "Imperative", "Declarative");
