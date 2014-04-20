dce1039b2787daefe5d823a4dfad602493eff71c
Laurent Weingart <laurent.weingart@epfl.ch>
2014-04-20 18:00:35 +0200

    cleans useless comments in devmine.js

--- site/assets/js/devmine.js
+++ site/assets/js/devmine.js
@@ -16,24 +16,3 @@ var options = new Array();
 	options[0] = "option 1";
 	options[1] = "option 2";
 	options[2] = "option 3";
-
-var bar = 'barbar';
-
-
-function Add_row(lang_value, slider_row){
-	$('#tab-lang').after('<tr><td>'+
-		lang_value+
-		'</td><td>'+
-		slider_row+
-		'</td></tr>');
-};
-
-/*
-var slider_row = "<input type=\"text\" class=\"span2\""+
- 									"value=\"4\" id=\"sl1\" style=\"\">"+
- 									"<script>$('#sl1').slider({formater: function(value) {
-											return 'Current value: '+value;
-										}
-									});
-								</script>";
-*/
