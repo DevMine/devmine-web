dac7af4eba1ed28805c1be33912e6f970d823dba
Laurent Weingart <laurent.weingart@epfl.ch>
2014-04-20 20:24:24 +0200

    adds sliders to paradigms and options, refactors devmine.js code

--- site/assets/js/devmine.js
+++ site/assets/js/devmine.js
@@ -1,18 +1,66 @@
 var languages = new Array();
-	languages[0] = "Java";
-	languages[1] = "Python";
-	languages[2] = "C++";
-	languages[3] = "Haskell";
+languages[0] = "Java";
+languages[1] = "Python";
+languages[2] = "C++";
+languages[3] = "Haskell";
 
 var paradigms = new Array();
-	paradigms[0] = "OOP";
-	paradigms[1] = "Functional";
-	paradigms[2] = "Symbolic";
-	paradigms[3] = "Logic";
-	paradigms[4] = "Imperative";
-	paradigms[5] = "Declarative";
+paradigms[0] = "OOP";
+paradigms[1] = "Functional";
+paradigms[2] = "Symbolic";
+paradigms[3] = "Logic";
+paradigms[4] = "Imperative";
+paradigms[5] = "Declarative";
 	
 var options = new Array();
-	options[0] = "option 1";
-	options[1] = "option 2";
-	options[2] = "option 3";
+options[0] = "option 1";
+options[1] = "option 2";
+options[2] = "option 3";
+
+$(function() {
+	
+	function add_slider(e) {
+		var $spin_value = $(e.data.id1).val();
+		var slider_id = e.data.id2;
+		var $table_id = $(e.data.id3);
+		var input_id = e.data.id4;
+		var slider = document.createElement('script');
+		slider.text = "$('"+slider_id+j+"').slider({formater: "+
+									"function(value){return 'Current value: '+value;}});";
+		$table_id.append('<tr><td>'+
+												$spin_value+
+												'</td><td id="sl'+j+
+												'"><input type="text" class="span2" value="4" id="'+input_id+j+
+												'" style="">'+
+												'</td></tr>');
+		$('#sl'+j+'')[0].appendChild(slider);
+		j++;
+	}
+	
+	/* languages */
+	$("input[name=languages]").TouchSpin({
+		items: languages
+	});
+	var i = 0;
+	var j = 0;
+	$('button#add-lang').click({
+		id1:'#spin-lang', id2:'#slid-ln', id3:'#tab-lang', id4:'slid-ln'
+	}, add_slider);
+	
+	/* paradigms */
+	$("input[name=paradigms]").TouchSpin({
+		items: paradigms
+	});
+	$('button#add-pdgm').click({
+			id1:'#spin-pdgm', id2:'#slid-pm', id3:'#tab-pdgm', id4:'slid-pm'
+		}, add_slider);
+
+	/* options */
+	$("input[name=options]").TouchSpin({
+		items: options
+	});
+	$('button#add-optn').click({
+		id1:'#spin-optn', id2:'#slid-op', id3:'#tab-optn', id4:'slid-op'
+	}, add_slider);
+		
+});
